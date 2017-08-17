ca7fa1a706f2e8934018e625575772f3d1a2cdc6
CauchyPeano <cauchypeano@github.com>
2017-08-17 22:44:51 +0200

    added project initial content

---
+++ src/main/java/de/cauchypeano/mstream/MstreamApplication.java
@@ -0,0 +1,12 @@
+package de.cauchypeano.mstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstreamApplication.class, args);
+	}
+}
---
+++ src/test/java/de/cauchypeano/mstream/MstreamApplicationTests.java
@@ -0,0 +1,16 @@
+package de.cauchypeano.mstream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MstreamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
